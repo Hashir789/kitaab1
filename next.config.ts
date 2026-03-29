7bda517469f9132edf675ab902db5b59fdc4511e
Muhammad Hashir Malik <m.hashir.malik1@gmail.com>
2026-03-29 17:36:22 +0500

    feature: configure next.config.ts to route to a single url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{ type: 'host', value: 'kitaab.me' }],
+        destination: 'https://www.kitaab.me/:1',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
